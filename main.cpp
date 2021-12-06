1cc3638e81f4627595213beaaf98b2bd0b5693a5
Disha Gaonkar <73191976+Dishagaonkar@users.noreply.github.com>
2021-12-06 17:32:08 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,9 +2,15 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <time.h>
+#include <chrono>
+#include <stdlib.h>
+#include <cctype>
 #include "edgeList.h"
 #include "adjacencyList.h"
+
 using namespace std;
+using namespace std::chrono;
 
 int main() {
 	cout << "Hello! welcome to Fast Flying Fairies, a COVID-19 conscious service to help you find the least crowded flight(s) to ceratin destinations in the U.S!" << endl;
@@ -16,17 +22,24 @@ int main() {
     Graph adjList;
     eList.readFile(); //data is put into the edge list
     adjList.readFile(); //data is put into the adjacency list
+    time_point<std::chrono::system_clock> start,end;
+    duration<double> elapsed_seconds;
+    
+    
 
 	while (x == true) {
-		cout << "What do you need help with?";
+		cout << "What do you need help with?" <<endl;
 		cout << "1. List of all airports available in our service" << endl;
 		cout << "2. Find airport route version 1" << endl;
         cout << "3. Find airport route version 2" << endl;
 		cout << "4. End the program" << endl;
 		cin >> choice;
+        
        
 		if (choice == 1) { //prints out all the airports
+
 			eList.printGraph(); 
+            //adjList.printGraph();
 		}
 
 		else if (choice == 2) { //return the route & passenger to seat ratio with each edge, edge list
@@ -34,6 +47,11 @@ int main() {
 			cin >> origin;
 			cout << "Now enter your destination" << endl;
 			cin >> destination;
+            start = system_clock::now();
+            eList.edgeListDijkstras(eList,origin, destination);
+            end = system_clock::now();
+            elapsed_seconds = end-start;
+            cout << "route to destination took " << elapsed_seconds.count() << " using edge list" <<endl;
 			//complete the graph algorithm to find the shortest route to destination
 		}
 
@@ -42,6 +60,11 @@ int main() {
 			cin >> origin;
 			cout << "Now enter your destination" << endl;
 			cin >> destination;
+            start = system_clock::now();
+            adjList.adjListDijkstras(adjList, origin, destination);
+            end = system_clock::now();
+            elapsed_seconds = end-start;
+            cout << "route to destination took " << elapsed_seconds.count() << " using adjlist list" <<endl;
 			//complete the graph algorithm to find the shortest route to destination
 		}
 
