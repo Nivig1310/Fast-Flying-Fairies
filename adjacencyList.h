4dc5e2b5d4c2a8ec4e846c6d523b8f73ba14af0c
Nivig1310 <70538476+Nivig1310@users.noreply.github.com>
2021-12-05 21:52:33 -0500

    Update adjacencyList.h

--- adjacencyList.h
+++ adjacencyList.h
@@ -8,13 +8,13 @@
 #include <map>
 #include <fstream>
 #include <set>
+#include<unordered_map>
 using namespace std;
 
 class Graph
 {
 public:
-    map<string, vector<pair<string,double>>> graph;
-    set<double> edges;
+    map<string, vector<pair<string, double>>> graph;
 public:
     void insertEdge(string from, string to, double weight);
     bool isEdge(string from, string to);
@@ -23,13 +23,22 @@ public:
     void printGraph();
     void readFile();
     double getLowestWeight(string from, string to);
+    void adjListDijkstras(Graph& Graph, string src, string to);
+    string helperLowest(Graph& Graph, set<string>& visted, unordered_map<string, double>& density);
+    void printRoute(unordered_map<string, string> parent, double avgDensity, string to);
+
+
+
+
+
+
 };
 
 void Graph::insertEdge(string from, string to, double weight)
 {
 
     graph[from].push_back(make_pair(to, weight));
-    if(graph.find(to)==graph.end()){
+    if (graph.find(to) == graph.end()) {
         graph[to] = {};
     }
 }
@@ -37,8 +46,8 @@ void Graph::insertEdge(string from, string to, double weight)
 bool Graph::isEdge(string from, string to)
 {
 
-    for(auto it = graph[from].begin(); it != graph[from].end(); it++){
-        if(it->first == to)
+    for (auto it = graph[from].begin(); it != graph[from].end(); it++) {
+        if (it->first == to)
             return true;
     }
     return false;
@@ -46,14 +55,14 @@ bool Graph::isEdge(string from, string to)
 
 vector<double> Graph::getWeight(string from, string to) //return -1 if no edge exists between from and to
 {
-    vector<double> weights = {-1};
+    vector<double> weights;
     int count = 0;
-    for(auto it = graph[from].begin(); it != graph[from].end(); it++){
-        if(it->first == to) {
-            if(count == 0){ //first weight
-                    count++;
-                    weights[0];
-                }
+    for (auto it = graph[from].begin(); it != graph[from].end(); it++) {
+        if (it->first == to) {
+            if (count == 0) { //first weight
+                count++;
+                weights[0];
+            }
             weights.push_back(it->second);
         }
     }
@@ -63,30 +72,31 @@ vector<double> Graph::getWeight(string from, string to) //return -1 if no edge e
 vector<string> Graph::getAdjacent(string vertex)
 {
     vector<string> neighbors;
-    for(auto it = graph[vertex].begin(); it != graph[vertex].end(); it++){
-        neighbors.push_back(it->first);
+    int x = graph[vertex].size();
+    for (int i = 0; i < graph[vertex].size(); i++) {
+        neighbors.push_back(graph[vertex].at(i).first);
     }
     sort(neighbors.begin(), neighbors.end());
 
     return neighbors;
 }
 
-double Graph::getLowestWeight(string from, string to){
+double Graph::getLowestWeight(string from, string to) {
     vector<double> weights = getWeight(from, to);
     double min = 1.0;
-    for(int i = 0; i < weights.size(); i++){
-        if(weights.at(i) < min)
+    for (int i = 0; i < weights.size(); i++) {
+        if (weights.at(i) < min)
             min = weights.at(i);
     }
     return min;
 
 }
 
-void Graph::printGraph(){
-    for(auto iter = graph.begin(); iter != graph.end(); iter++){
+void Graph::printGraph() {
+    for (auto iter = graph.begin(); iter != graph.end(); iter++) {
         cout << endl;
         cout << iter->first;
-        for(int i = 0; i < graph[iter->first].size(); i++){
+        for (int i = 0; i < graph[iter->first].size(); i++) {
             cout << " " << graph[iter->first].at(i).first;
         }
         cout << endl;
@@ -94,17 +104,16 @@ void Graph::printGraph(){
 }
 
 
-void Graph::readFile(){
+void Graph::readFile() {
     ifstream file("Airports2.csv");
     if (file.is_open()) {
 
         //step 0. dealing w first line of data
         string FirstLine;
         getline(file, FirstLine);
-        cout << FirstLine;
         //
         int i = 0;
-        while(getline(file, FirstLine) && i < 100000){
+        while (getline(file, FirstLine) && i < 100000) {
             //create a stream from line of data
             istringstream stream(FirstLine);
             string origin_air;
@@ -118,19 +127,115 @@ void Graph::readFile(){
             double result = 0.0;
 
             getline(stream, origin_air, ',');
-            getline(stream,destination_air, ',');
+            getline(stream, destination_air, ',');
             getline(stream, origin_city, ',');
             getline(stream, dest_city, ',');
             getline(stream, bs, ',');
             getline(stream, bs2, ',');
             getline(stream, passengers, ',');
             getline(stream, seats, ',');
-            result = ((double)stoi(passengers)/(double)stoi(seats));
-            insertEdge(origin_air,destination_air,result);
+            result = ((double)stoi(passengers) / (double)stoi(seats));
+            origin_air = origin_air.substr(1, 3);
+            destination_air = destination_air.substr(1, 3);
+            insertEdge(origin_air, destination_air, result);
             //now each of these variables holds the values u need.
             i++;
         }
     }
 
-};
+}
+
+string Graph::helperLowest(Graph& Graph, set<string>& visted, unordered_map<string, double>& density) { //
+    int min = INT_MAX;
+    string index = "";
+    for (auto it = density.begin(); it != density.end(); it++) {
+        if (min > density[it->first] && visted.find(it->first) == visted.end()){
+            min = density[it->first];
+            index = it->first;
+        }
+    }
+    return index;
+}
+
+
+
+ void Graph::adjListDijkstras(Graph& Graph, string src, string to) {
+    set<string> visited;
+    set<string> needToProcess;
+    unordered_map<string, double> density; //replaced d, previously vector<int> d
+    unordered_map<string, string> parent;
+    //puts all vertices in need to process
+    for (auto it = Graph.graph.begin(); it != Graph.graph.end(); it++) {
+        needToProcess.insert(it->first);
+        density[it->first] = INT_MAX;
+        parent[it->first] = "";
+    }
+
+    density[src] = 0;
+    parent[src] = "";
+
+    visited.insert(src);
+    needToProcess.erase(src);
+    vector<string> neighbors = Graph.getAdjacent(src);
+
+    //loop through neighbors of src and set their lowest weights as their distances. Set parent
+    for (auto it = neighbors.begin(); it != neighbors.end(); ++it) {
+        density[*it] = Graph.getLowestWeight(src, *it);
+        parent[*it] = src;
+    }
+
+    string current = src;
+
+
+
+    while (needToProcess.empty() == false) { //while needtoprocess isnt empty
+        if (current == "") {
+            break;
+        }
+        vector<string> x =  Graph.getAdjacent(current);
+        for (auto it = x.begin(); it != x.end(); it++) {
+            if ((density[current] + Graph.getLowestWeight(current, *it)) < density[*it]) { //relaxation
+                density[*it] = density[current] + Graph.getLowestWeight(current, *it);
+                parent[*it] = current;
+            }
+        }
+        needToProcess.erase(current);
+        visited.insert(current);
+        current = Graph.helperLowest(Graph, visited, density);
+    }
+     printRoute(parent, density[to], to);
+}
+
+void Graph::printRoute(unordered_map<string, string> parent, double avgDensity, string to){
+    if(parent[to] == "") //if no connection exists between origin and destination
+        cout << "No connection from the origin airport to the desired destination airport" << endl;
+
+    else{ //if the connection exiss
+        stack<string> airports; //take in the parents starting from the destination all the way to the source
+        airports.push(to); //start from the destination
+        string cur = to;
+
+        while(cur != ""){ //cur is only "" if the source has been added into the stack already
+            if(parent[cur] != "")
+                airports.push(parent[cur]);
+
+            cur = parent[cur];
+        }
+
+        avgDensity = avgDensity / airports.size(); //set the average density to the correct value
+
+        cout << "Your route is: ";
+        while(airports.size() > 0){
+            if(airports.size() == 1) //if it's going to print out the destination airport
+                cout << airports.top() << endl;
+            else
+                cout << airports.top() << "-->";
+
+            airports.pop();
+        }
+
+        cout << "The average density (passenger to available seats ratio) of this route is: " << avgDensity << endl;
+    }
+}
+
 
